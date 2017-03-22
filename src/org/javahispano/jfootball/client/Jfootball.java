4b77aae31a4585718046c67a6ef6cb2727de8478
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2017-03-22 18:34:52 +0100

    Eliminado panel lateral

--- src/org/javahispano/jfootball/client/Jfootball.java
+++ src/org/javahispano/jfootball/client/Jfootball.java
@@ -1,7 +1,13 @@
 package org.javahispano.jfootball.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.javahispano.jfootball.client.application.animations.AnimationDemo;
 
+import com.akjava.gwt.bvh.client.BoxData;
+import com.akjava.gwt.three.client.js.core.Object3D;
+import com.akjava.gwt.three.client.js.math.Vector3;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
@@ -17,46 +23,21 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Jfootball implements EntryPoint {
-	private DockLayoutPanel center;
+	private DockLayoutPanel root;
 	private AnimationDemo animationDemo;
+	
+	private Map<String, Vector3> boneSizeMap = new HashMap<String, Vector3>();
+	private Object3D rootGroup, boneContainer, backgroundContainer;
+	private Map<String, BoxData> boxDatas;
+
 
 	@Override
 	public void onModuleLoad() {
-		DockLayoutPanel root = new DockLayoutPanel(Unit.PX);
-
+		root = new DockLayoutPanel(Unit.PX);
+		
 		RootLayoutPanel.get().add(root);
 
-		center = new DockLayoutPanel(Unit.PX);
-
-		// left-side
-		VerticalPanel side = new VerticalPanel();
-		side.setWidth("100%");
-		side.setStylePrimaryName("side");
-		side.setSpacing(4);
-
-		Label title = new Label("GWT-three.js Examples");
-		title.setStylePrimaryName("header");
-
-		side.add(title);
-
-		Label webgl = new Label("webgl");
-		webgl.setStylePrimaryName("subheader");
-		webgl.setWidth("100%");
-		side.add(webgl);
-		root.addWest(side, 300);
-		root.add(center);
-
-		Label links = new Label("Links");
-		links.setStylePrimaryName("subheader");
-		side.add(links);
-		side.add(new Anchor("Three.js(github)", "https://github.com/mrdoob/three.js/"));
-		side.add(new Anchor("Three.js origin examples", "http://threejs.org/examples/"));
-		side.add(new Anchor("GWT-Three.js(github)", "https://github.com/akjava/gwt-three.js-test"));
-		side.add(new Anchor("GWT-Three.js old examples", "http://akjava.github.io/gwt-three.js-test/ThreeTest.html"));
-		side.add(new Anchor("GWT", "http://www.gwtproject.org/"));
-
 		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-
 			@Override
 			public void execute() {
 
@@ -67,7 +48,7 @@ public class Jfootball implements EntryPoint {
 	}
 
 	public Panel getPanel() {
-		return center;
+		return root;
 	}
 
 }
