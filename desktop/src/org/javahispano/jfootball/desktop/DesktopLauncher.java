ef8a89ed5475f1f9605d50f6569955d1f7aa3b20
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2017-05-04 15:55:20 +0200

    First commit!

---
+++ desktop/src/org/javahispano/jfootball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.javahispano.jfootball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.javahispano.jfootball.Jfootball;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Jfootball(), config);
+	}
+}
